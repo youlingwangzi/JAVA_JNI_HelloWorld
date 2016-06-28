2e405792b211b500832dd6ad5ed9ea5eab455d1f
yuanxiaokun <yuanxiaokun@outlook.com>
2016-06-29 01:02:39 +0800

    增加部分注释

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,11 @@
-class HelloWorld 
+class HelloWorld
 {
+    //native数据
     public native void displayHelloWorld();
     static {
         System.loadLibrary("hello");
     }
-    
+
     public static void main(String[] args) {
         new HelloWorld().displayHelloWorld();
     }
